8a0c1d6bbcd03f0fee2229aa670d073fbbe1b0cb
Thapelo-ST <tsamkelo96@gmail.com>
2023-08-02 00:41:04 +0200

    rublar 4

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+def match_school(input)
+  # insert delim to be found
+  pattern = /^h(b+t*+n|n)/
+
+  # match using oniguruma lib
+  matches = input.scan(pattern)
+
+  if matches.empty?
+    puts ""
+  else
+    puts "#{matches.join('')}"
+  end
+end
+
+
+# validate the arguments
+if ARGV.length != 1
+  puts "Ussage: ruby match_school.rb <input text>"
+else
+  match_school(ARGV[0])
+end
