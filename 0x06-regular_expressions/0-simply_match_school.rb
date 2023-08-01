1ba1746e91e7423ecc0328b2f4d60fa3be223f07
Thapelo-ST <tsamkelo96@gmail.com>
2023-08-01 23:30:08 +0200

    test

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+# importing Oniguruma lib
+#require 'oniguruma'
+require 'oniguruma'
+
+def match_school(input)
+  # insert delim to be found
+  pattern = /school/
+
+  # match using oniguruma lib
+  matches = input.scan(pattern)
+
+  if matches.empty?
+    puts "No matches found"
+  else
+    puts "#{matches.join(', ')}"
+  end
+end
+
+
+# validate the arguments
+if ARGV.length != 1
+  puts "Ussage: ruby match_school.rb <input text>"
+else
+  match_school(ARGV[0])
+end
