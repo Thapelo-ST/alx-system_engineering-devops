5a090f3de79b04562e3cec5f4449d1415f14e54f
Thapelo-ST <tsamkelo96@gmail.com>
2023-09-22 07:04:14 -0700

    fix 1

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 exec { 'killmenow_process':
-command     => '/usr/bin/pkill -f killmenow',
-path        => '/usr/bin',
-onlyif      => '/usr/bin/pgrep -f killmenow',
-refreshonly => true,
+  command     => '/usr/bin/pkill -f killmenow',
+  path        => '/usr/bin',
+  onlyif      => '/usr/bin/pgrep -f killmenow',
+  refreshonly => true,
 }
