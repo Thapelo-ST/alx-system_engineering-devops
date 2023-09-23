1cc92e6769939ba57c74ec3bde81f9a00a35f88f
Thapelo-ST <tsamkelo96@gmail.com>
2023-09-22 18:43:09 -0700

    final fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # Execution file that kills the process KILLMENOW
 
 exec { 'killmenow_process':
-  command     => '/usr/bin/pkill -f killmenow',
+  command     => '/usr/bin/pkill killmenow',
   path        => '/usr/bin',
-  onlyif      => '/usr/bin/pgrep -f killmenow',
+  onlyif      => '/usr/bin/pgrep killmenow',
   refreshonly => true,
 }
